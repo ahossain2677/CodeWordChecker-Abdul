8e2fec6eb71bda27f2b65c9d5fe2404398074c26
BT_1N3_08@BKTECH.LOCAL <BT_1N3_08@BKTECH.LOCAL>
2018-11-26 08:40:40 -0500

    Initial commit

---
+++ src/CodeWordChecker.java
@@ -0,0 +1,13 @@
+public class CodeWordChecker
+{
+
+    this.StringChecker sc1 = new CodeWordChecker(int min ,int max ,char symbol);
+
+    int min = 5 ;
+    int max = 8 ;
+    char symbol = "$";
+
+
+    this.StringChecker sc2 = new CodeWordChecker(char symbol)
+    char symbol = "pass";
+}
---
+++ src/com/company/CodeWordRunner.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class CodeWordRunner
+{
+
+    public static void main(String[] args)
+    {
+        StringChecker sc1 = new codeWordChecker (min:5, max:6, symbol:"$");
+        StringChecker sc2 = new codeWordChecker (symbol:"pass");
+
+        System.out.println(sc1.isValid("happy"));
+        System.out.println(sc1.isValid("happy$"));
+        System.out.println(sc1.isValid("Code"));
+        System.out.println(sc1.isValid("happyCode"));
+
+        System.out.println(sc2.isValid("Mypass"));
+        System.out.println(sc2.isValid("MypassPort"));
+        System.out.println(sc2.isValid("happy"));
+        System.out.println(sc2.isValid("1,000,000,000,000,000"));
+    }
+}
